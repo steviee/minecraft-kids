e4dd8b85b536d8f676a2ee29eb863a90c72e69fb
Stephan Eberle <stephan.eberle@xing.com>
2025-10-30 13:11:04 +0100

    Fix frontend ESLint config for CI
    - Update to proper ESLint 9 flat config format
    - Use installed packages (@typescript-eslint/eslint-plugin, @typescript-eslint/parser)
    - Fix import syntax for vue-eslint-parser
    - Remove undefined config references
    
    Frontend linter now passes without errors.

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,29 +1,38 @@
 import vue from 'eslint-plugin-vue';
-import tsEslint from '@typescript-eslint/eslint-plugin';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
-import vueParser from 'vue-eslint-parser';
 
 export default [
   {
-    files: ['src/**/*.ts', 'src/**/*.vue'],
+    files: ['**/*.vue', '**/*.ts', '**/*.tsx'],
     languageOptions: {
-      parser: vueParser,
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      vue,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: (await import('vue-eslint-parser')).default,
       parserOptions: {
         parser: tsParser,
         ecmaVersion: 2022,
         sourceType: 'module',
-        project: './tsconfig.json',
       },
     },
-    plugins: {
-      vue,
-      '@typescript-eslint': tsEslint,
-    },
     rules: {
-      ...vue.configs['vue3-recommended'].rules,
-      ...tsEslint.configs.recommended.rules,
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      '@typescript-eslint/explicit-function-return-type': 'off',
       'vue/multi-word-component-names': 'off',
     },
   },
